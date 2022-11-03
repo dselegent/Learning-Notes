e88befa892b8fcb00e9850e1d3557ff3339855fa
dselegent <1799661558@qq.com>
2022-11-03 16:53:36 +0800

    add config script

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,11 +11,7 @@ export default defineUserConfig({
   title: 'dselegent blog',
   description: 'dselegent的博客站点',
   head: [
-    [
-      'meta',
-      { name: 'referrer', content: 'no-referrer' },
-      { name: 'baidu-site-verification', content: 'code-iW58SZ4SVS' },
-    ],
+    ['meta', { name: 'referrer', content: 'no-referrer' }],
     ['meta', { name: 'baidu-site-verification', content: 'code-iW58SZ4SVS' }],
     [
       'link',
@@ -28,16 +24,39 @@ export default defineUserConfig({
       'script',
       {},
       `
-  var _hmt = _hmt || [];
-  (function() {
+    var _hmt = _hmt || [];
+    (function() {
     var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?xcsafasdffsadfafd";
+    hm.src = "https://hm.baidu.com/hm.js?**********************";
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(hm, s);
-  })();
-  `,
+
+    // 引入谷歌,不需要可删除这段
+    var hm1 = document.createElement("script");
+    hm1.src = "https://www.googletagmanager.com/gtag/js?id=UA-00000000-1";
+    var s1 = document.getElementsByTagName("script")[0];
+    s1.parentNode.insertBefore(hm1, s1);
+    })();
+
+    // 谷歌加载,不需要可删除
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-00000000-1');
+`,
     ],
-    ['script', { src: 'https://res.wx.qq.com/open/js/jweixin-1.6.0.js' }],
+    [
+      'script',
+      {},
+      `
+      (function() {
+         if (location.href.indexOf('github.io') > -1 || location.href.indexOf('gitee.io') > -1) {
+             location.href = 'https://ts.yayujs.com'
+         }
+      })();
+      `,
+    ],
   ],
   theme: defaultTheme({
     navbar: [
